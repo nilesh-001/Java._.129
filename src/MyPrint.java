791955b0b51a5c82c4df62741a78cfe7a28550df
Nilesh Singh Yadav <290905nilesh@gmail.com>
2025-08-22 11:38:48 +0530

    Code till 22-Aug

---
+++ src/MyPrint.java
@@ -0,0 +1,9 @@
+public class MyPrint implements Printtable,Showable {
+    public void print(){
+        System.out.println("Print method is implemented.");
+    }
+   public void show(){
+       System.out.println("Show method is implemented.");
+    }
+
+}
---
+++ src/Printtable.java
@@ -0,0 +1,9 @@
+public interface Printtable {
+    void print();
+//    private void my(){
+//        System.out.println("This id my private method");
+//    }
+//    public static void myStatic(){
+//        System.out.println("This is my default static method");
+//    }
+}

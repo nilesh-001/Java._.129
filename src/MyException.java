5dc9a03be976d751355fc7684136878da33f7647
Nilesh Singh Yadav <290905nilesh@gmail.com>
2025-08-27 14:26:05 +0530

    Final codes of 27-Aug

--- src/MyException.java
+++ src/MyException.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class MyException {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int x,y,z=0;
-
         Scanner sc=new Scanner(System.in);
         try{
             x=sc.nextInt();

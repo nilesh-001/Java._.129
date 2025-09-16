15a23bd9756194ff0c63f7cdf2b63b5d02160c58
nilesh-001 <290905nilesh@gmail.com>
2025-09-16 12:26:47 +0530

    codes till 16th september

---
+++ src/Unit3/Sept16/hashMap1.java
@@ -0,0 +1,20 @@
+package Unit3.Sept16;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class hashMap1 {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> hm=new LinkedHashMap<>();
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            String key=sc.next();
+            int val=sc.nextInt();
+            hm.put(key, val);
+        }
+        for(String str : hm.keySet()){
+            System.out.println(str+" "+ hm.get(str));
+        }
+    }
+}

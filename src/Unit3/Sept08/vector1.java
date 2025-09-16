2355e0f53cb83a859cafb64cfa54ca623ca361e0
nilesh-001 <290905nilesh@gmail.com>
2025-09-16 09:32:23 +0530

    updated codes till 12th sept

---
+++ src/Unit3/Sept08/vector1.java
@@ -0,0 +1,15 @@
+package Unit3.Sept08;
+
+import java.util.*;
+
+public class vector1 {
+    public static void main(String[] args) {
+        Vector<String> vec = new Vector<>(3,2);
+        vec.add("First");
+        vec.add("2nd");
+        vec.add("3rd");
+        vec.add("4th");
+        System.out.println(vec.capacity());
+        System.out.println(vec.size());
+    }
+}
